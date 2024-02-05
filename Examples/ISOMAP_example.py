195a6c82c832c24e19d6597e08f0af4f6727217d
Theodoulos Rodosthenous <45739717+theorod93@users.noreply.github.com>
2024-02-05 00:50:14 +0000

    Update ISOMAP_example.py

--- Examples/ISOMAP_example.py
+++ Examples/ISOMAP_example.py
@@ -29,7 +29,7 @@ Xinput[5] = Xf
 
 start_time_multiISOMAP = time.time()
 # Run algorithm
-Y_multiISOMAP = multiISOMAP(Xinput, 2, 50, 20.0, 1000)
+Y_multiISOMAP = multi_isomap_path(Xinput, 2, 50, 20.0, 1000)
 end_time_multiISOMAP = time.time()
 # CoISOMAPct computational time
 running_time_multiISOMAP = end_time_multiISOMAP - start_time_multiISOMAP
@@ -45,7 +45,7 @@ Xinput[5] = Xf
 
 start_time_mISOMAP = time.time()
 # Run algorithm
-Y_mISOMAP = mISOMAP(Xinput, 2, 50, 10.0, 1000)
+Y_mISOMAP = m_isomap(Xinput, 2, 50, 10.0, 1000)
 end_time_mISOMAP = time.time()
 # CoISOMAPct computational time
 running_time_mISOMAP = end_time_mISOMAP - start_time_mISOMAP
